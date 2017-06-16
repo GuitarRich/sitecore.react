ec2a817e19b5b152d9efaf2560bb7e69980c207b
Richard Seal <richard.seal@rbaconsulting.com>
2017-06-15 21:52:26 -0500

    Another attempt at sorting the pack ps file paths

--- pack.ps1
+++ pack.ps1
@@ -1,12 +1,13 @@
-$version = [System.Reflection.Assembly]::LoadFile(".\src\Sitecore.React\bin\Release\Sitecore.React.dll").GetName().Version
+$root = (split-path -parent $MyInvocation.MyCommand.Definition)
+$version = [System.Reflection.Assembly]::LoadFile("$($root)\src\Sitecore.React\bin\Release\Sitecore.React.dll").GetName().Version
 $versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
 
 Write-Host "Setting .nuspec version tag to $versionStr"
 
-$content = (Get-Content .\build\Sitecore.React.Nuget\Sitecore.React.Web.nuspec) 
+$content = (Get-Content $root\build\Sitecore.React.Nuget\Sitecore.React.Web.nuspec) 
 $content = $content -replace '\$version\$',$versionStr
 
-$content | Out-File .\Build\Sitecore.React.Web.nuspec
+$content | Out-File $root\Build\Sitecore.React.Web.nuspec
 
-& $root\NuGet\NuGet.exe pack .\Build\Sitecore.React.Nuget\Sitecore.React.nuspec
-& $root\NuGet\NuGet.exe pack .\Build\Sitecore.React.Nuget\Sitecore.React.Web.nuspec
+& $root\NuGet\NuGet.exe pack $root\Build\Sitecore.React.Nuget\Sitecore.React.nuspec
+& $root\NuGet\NuGet.exe pack $root\Build\Sitecore.React.Nuget\Sitecore.React.Web.nuspec
