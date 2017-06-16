fc6bc13050d0a254dab2f89a1518cc49b8090ef1
Richard Seal <richard.seal@rbaconsulting.com>
2017-06-15 21:59:43 -0500

    Another attempt at sorting the pack ps file paths

--- pack.ps1
+++ pack.ps1
@@ -4,10 +4,5 @@ $versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
 
 Write-Host "Setting .nuspec version tag to $versionStr"
 
-$content = (Get-Content $root\build\Sitecore.React.Nuget\Sitecore.React.Web.nuspec) 
-$content = $content -replace '\$version\$',$versionStr
-
-$content | Out-File $root\Build\Sitecore.React.Web.nuspec
-
-& $root\NuGet\NuGet.exe pack $root\Build\Sitecore.React.Nuget\Sitecore.React.nuspec
-& $root\NuGet\NuGet.exe pack $root\Build\Sitecore.React.Nuget\Sitecore.React.Web.nuspec
+& $root\NuGet\NuGet.exe pack $root\Build\Sitecore.React.Nuget\Sitecore.React.nuspec -Version $versionStr
+& $root\NuGet\NuGet.exe pack $root\Build\Sitecore.React.Nuget\Sitecore.React.Web.nuspec -Version $versionStr
