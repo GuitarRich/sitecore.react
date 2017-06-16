7d8573911152069341f56ce150becdc8b0f19eb8
Richard Seal <richard.seal@rbaconsulting.com>
2017-06-16 15:44:47 -0400

    More build fixes for auto generating nupkg

--- pack.ps1
+++ pack.ps1
@@ -5,4 +5,3 @@ $versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
 Write-Host "Setting .nuspec version tag to $versionStr"
 
 & $root\NuGet\NuGet.exe pack $root\Build\Sitecore.React.Nuget\Sitecore.React.nuspec -Version $versionStr
-& $root\NuGet\NuGet.exe pack $root\Build\Sitecore.React.Nuget\Sitecore.React.Web.nuspec -Version $versionStr
