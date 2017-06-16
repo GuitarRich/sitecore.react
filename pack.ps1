d577866a678c7b8fa9b45b13966ea9f98de1e671
Richard Seal <richard.seal@rbaconsulting.com>
2017-06-15 21:55:52 -0500

    Another attempt at sorting the pack ps file paths

--- pack.ps1
+++ pack.ps1
@@ -1,5 +1,5 @@
 $root = (split-path -parent $MyInvocation.MyCommand.Definition)
-$version = [System.Reflection.Assembly]::LoadFile("$($root)\src\Sitecore.React\bin\Release\Sitecore.React.dll").GetName().Version
+$version = [System.Reflection.Assembly]::LoadFile("$($root)\src\Sitecore.React\bin\Sitecore.React.dll").GetName().Version
 $versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
 
 Write-Host "Setting .nuspec version tag to $versionStr"
