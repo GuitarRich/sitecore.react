8b621d9a9ac28d63269f81837c63691b6bb49b27
Richard Seal <richard.seal@rbaconsulting.com>
2017-06-15 21:43:56 -0500

    Setting up automatic deployments for nuget packages

---
+++ pack.ps1
@@ -0,0 +1,13 @@
+$root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
+$version = [System.Reflection.Assembly]::LoadFile("$root\src\Sitecore.React\bin\Release\SitecoreReact.dll").GetName().Version
+$versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
+
+Write-Host "Setting .nuspec version tag to $versionStr"
+
+$content = (Get-Content $root\NuGet\Sitecore.React.Web.nuspec) 
+$content = $content -replace '\$version\$',$versionStr
+
+$content | Out-File $root\Build\Sitecore.React.Web.nuspec
+
+& $root\NuGet\NuGet.exe pack $root\Build\Sitecore.React.nuspec
+& $root\NuGet\NuGet.exe pack $root\Build\Sitecore.React.Web.nuspec
