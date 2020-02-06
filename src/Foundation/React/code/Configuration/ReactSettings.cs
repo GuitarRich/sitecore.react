90c9146c42793c7257f9402cda3efb5655026ac7
Roydon Gyles-Bedford <roydon.gyles-bedford@perficient.com>
2020-02-06 08:20:04 -0600

    Add configuration option to use CamelCasePropertyNamesContractResolver when serializing json

--- src/Foundation/React/code/Configuration/ReactSettings.cs
+++ src/Foundation/React/code/Configuration/ReactSettings.cs
@@ -5,6 +5,7 @@ namespace Foundation.React.Configuration
         public bool UseDebugReactScript { get; set; }
         public string DynamicPlaceholderType { get; set; }
         public string BundleName { get; set; }
+        public bool UseCamelCasePropertyNames { get; set; }
         public bool EnableClientSide { get; set; }
         public bool DisableClientSideWhenEditing { get; set; }
         public string BundleType { get; set; }
